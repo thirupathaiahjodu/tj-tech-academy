f7546172a2ba24c932d856f154d7a880b55fd417
thirupathaiahjodu <152474765+thirupathaiahjodu@users.noreply.github.com>
2026-03-09 10:36:49 +0530

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -99,7 +99,7 @@ USE_I18N = True
 USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
